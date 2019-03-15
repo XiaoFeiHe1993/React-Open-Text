86b0a6fbe46015e59a2a58d0454c44acf9654355
hexiaofei <hexiaofei@dxy.cn>
2019-03-15 11:42:49 +0800

    first commit

---
+++ examples/simple.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TextOpen from 'text-open';
+import 'text-open/assets/index.less';
+
+ReactDOM.render(
+  <div style={{ margin: '20px 20px 20px' }}>
+    <TextOpen showText="鲁迅一生在文学创作、文学批评、思想研究、文学史研究、翻译、美术理论引进、基础科学介绍和古籍校勘与研究等多个领域具有重大贡献。他对于五四运动以后的中国社会思想文化发展具有重大影响，蜚声世界文坛，尤其在韩国、日本思想文化领域有极其重要的地位和影响，被誉为“二十世纪东亚文化地图上占最大领土的作家”。鲁迅一生在文学创作、文学批评、思想研究、文学史研究、翻译、美术理论引进、基础科学介绍和古籍校勘与研究等多个领域具有重大贡献。他对于五四运动以后的中国社会思想文化发展具有重大影响，蜚声世界文坛，尤其在韩国、日本思想文化领域有极其重要的地位和影响"
+      openText="展开"
+      collapseText="收起"
+      lineMax={35}/>
+  </div>,
+  document.getElementById('__react-content'),
+);
